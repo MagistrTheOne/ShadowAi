cef18bbebd8db569942c0ee9f2b2801bdd3ebf04
MagistrTheOne <maxonyushko71@gmail.com>
2025-07-19 22:36:02 +0300

    microf

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -3,11 +3,10 @@
  import { useQuery } from '@tanstack/react-query';
 
  export const HomeView = () => {
-    const trpc =useTRPC ();
-    const {data} = useQuery(trpc.hello.queryOptions({text: "MagistrTheOne"}));
+   
     return(
-        <div className='flex flex-col p-4 gap-y-4'>
-        {data?.greeting}
+        <div >
+            Home view
         </div>
     )
  }
