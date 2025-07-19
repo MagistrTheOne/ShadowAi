dd2a78ba21d95f1eaf58a8a73d53b84df88ad417
MagistrTheOne <maxonyushko71@gmail.com>
2025-07-19 21:15:08 +0300

    micro-feature

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -7,7 +7,7 @@
     const {data} = useQuery(trpc.hello.queryOptions({text: "MagistrTheOne"}));
     return(
         <div className='flex flex-col p-4 gap-y-4'>
-        {data?.greeting};
+        {data?.greeting}
         </div>
     )
  }
